92a189a6fa9f2a04205698f6688ba051748e822a
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2011-11-03 12:56:12 +0400

    artifact added

--- src/org/myapp/App.java
+++ src/org/myapp/App.java
@@ -1,10 +1,9 @@
 package org.myapp;
 
-import com.sun.jna.Native;
-
 public class App {
+  static MyLib lib = MyLib.INSTANCE;
+
   public static void main(String[] args) {
-    MyLib lib = (MyLib) Native.loadLibrary("my", MyLib.class);
     lib.register_callback(new MyCallBack());
 
     System.out.println(lib.calculate_str_len("string for calculation"));
@@ -12,5 +11,17 @@ public class App {
     System.out.println(lib.get_one());
 
     System.out.println(lib.get_two());
+  }
+
+  public static void print() {
+    System.out.println(lib.get_two());
+  }
+
+  public int get2() {
+    return lib.get_two();
+  }
+
+  public static int get3() {
+    return lib.get_two();
   }
 }
--- src/org/myapp/MyLib.java
+++ src/org/myapp/MyLib.java
@@ -1,8 +1,11 @@
 package org.myapp;
 
 import com.sun.jna.Library;
+import com.sun.jna.Native;
 
 public interface MyLib extends Library {
+  public MyLib INSTANCE = (MyLib) Native.loadLibrary("my", MyLib.class);
+
   int calculate_str_len(String name);
 
   void register_callback(MyCallBack myc);
